3408be84019fc6887a408e9ddbb8b763f317f40e
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2018-07-04 16:22:22 +0700

    :sparkles: Custom folder to make it tidy

--- next.config.js
+++ next.config.js
@@ -1,59 +1,6 @@
-const path = require('path');
-const glob = require('glob');
+const webpackConfig = require('./webpack.config');
 
 module.exports = {
-  webpack: (config) => {
-    // Fixes npm packages that depend on `fs` module
-
-    const configuration = config;
-
-    configuration.node = {
-      fs: 'empty'
-    };
-
-    configuration.module.rules = [
-      ...config.module.rules,
-      {
-        test: /\.css$/,
-        use: ['babel-loader?compact=false', 'raw-loader', {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true
-          }
-        }]
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: ['babel-loader?compact=false', 'raw-loader', {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: 'inline'
-          }
-        }, {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-            includePaths: ['styles', 'node_modules']
-              .map(d => path.join(__dirname, d))
-              .map(g => glob.sync(g))
-              .reduce((a, c) => a.concat(c), [])
-          }
-        }
-        ]
-      }
-    ];
-
-    /* If in Dev Environment, add eslint */
-    config.module.rules.push({
-      test: /\.js$/,
-      enforce: 'pre',
-      exclude: [/node_modules/],
-      loader: 'eslint-loader',
-      options: {
-        emitWarning: true
-      }
-    });
-
-    return config;
-  }
+  dir: './src',
+  webpack: (config, { dev }) => webpackConfig(config, { dev })
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App, { Container } from 'next/app';
 import withRedux from 'next-redux-wrapper';
-import initialStores from '../redux/stores';
+import initialStores from 'redux/stores';
 
 class MyApp extends App {
   render() {
